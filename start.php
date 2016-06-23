253ad6fbb2be8ef8cd78f39043a5946e7cd03b23
vagrant <vagrant@localhost.localdomain>
2016-06-23 10:52:00 +0900

    色々変更

--- start.php
+++ start.php
@@ -1,9 +1,13 @@
 <?php
     require_once("common.php");
-    if(!is_null($_GET["watch"])){
+    if(!is_null($_GET["start"])){
         $sconnection = ssh2_connect(ADDRESS, 22);
         ssh2_auth_password($sconnection, USER, PASSWORD);
-        $command = LEDTEST." ".$_GET["watch"];
+	if($_GET["start"] === 1){
+            $command = "";
+	}else if($_GET["start"] === 0){
+	    $command = "";
+	}
         $stdio_stream = ssh2_exec($sconnection, $command);
     }
 ?>
--- takePicture.php
+++ takePicture.php
@@ -1,15 +1,15 @@
 <?php
     require_once("./exportPicture.php");
-    require_once("common.php");
+    require_once("./common.php");
 
     $filename = date('Y-m-d_G-i-s').".jpg";
-    touch($filename);
+    touch("./img/".$filename);
     if(!is_null($_GET["take"])){
         $sconnection = ssh2_connect(ADDRESS, 22);
         ssh2_auth_password($sconnection, USER, PASSWORD);
-        $command = "raspistill -o ".$filename;
+        $command = "raspistill -o ./img/".$filename;
         $stdio_stream = ssh2_exec($sconnection, $command);
     }
     $dropbox = new DropBox();
-    $dropbox->Upload($filename, "/LOCAL/".$filename);
+    $dropbox->Upload("./img/".$filename, "/LOCAL/".$filename);
 ?>
