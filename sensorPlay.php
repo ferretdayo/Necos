c2d5b0e716c9a833a9ac80bc70a26cbe739dd275
ferretdayo <ferretdayo@hotmail.co.jp>
2016-06-25 13:29:06 +0000

    create sensorPlay

---
+++ sensorPlay.php
@@ -0,0 +1,8 @@
+<?php
+	require_once("./common.php");
+	
+	$sconnection = ssh2_connect(ADDRESS, 22);
+        ssh2_auth_password($sconnection, USER, PASSWORD);
+	$sensorCommand = "/usr/bin/sudo /home/pi/public_html/Necos/sensorPin";
+	ssh2_exec($sconnection, $sensorCommand);
+?>
